88e780912c3caed9ff77ae52b79b1edbee2f1012
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2023-07-01 14:13:55 +0200

    feat: add autoredirect to sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import Image from "next/image";
 
-import { Provider } from "react-redux";
-import { store } from "redux/store";
+import {useGetAuthMeQuery} from "../api/auth.api";
+import {redirect} from "next/navigation";
 
 
 export default function Home() {
+  const {isSuccess} = useGetAuthMeQuery()
+  if(!isSuccess){redirect('/sign-in')}
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
