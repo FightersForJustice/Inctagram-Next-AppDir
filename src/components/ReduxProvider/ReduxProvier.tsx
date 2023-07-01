bde5bed150b9868494a552e19bdfb5f2a055fa8d
denis <warning11223@gmail.com>
2023-07-01 13:43:54 +0300

    feat: add global provider

---
+++ src/components/ReduxProvider/ReduxProvier.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
+import React, { PropsWithChildren } from "react";
+
+export const ReduxProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
