98fe338cf12fba3eea4bce0406a4246f6a0bd89c
warning11223 <warning11223@gmail.com>
2023-08-04 15:49:33 +0300

    fix: https://inctagram.work/api/v1 change backend api

--- src/api/posts.api.ts
+++ src/api/posts.api.ts
@@ -75,7 +75,7 @@ export const postsApi = createApi({
     getPosts: builder.query<PostsWithPagination, number>({
       query: (pageNumber) => {
         return {
-          url: `posts`,
+          url: `posts?pageNumber=${pageNumber}`,
           method: "GET",
         };
       },
