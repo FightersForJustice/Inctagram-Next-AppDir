f3e6da18b68bb8a3bd02a719d4e271da442aaa8c
warning11223 <warning11223@gmail.com>
2023-08-05 23:35:57 +0300

    fix: https://inctagram.work/api/v1 change backend api

--- src/app/[locale]/sign-in/page.tsx
+++ src/app/[locale]/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import SignIn from "./SignIn/SignIn";
 import { useTranslations } from "next-intl";
