f5f1f6d087bf3da94eabcd61a492fa7f4741e29e
warning11223 <warning11223@gmail.com>
2023-08-07 20:06:18 +0300

    fix: "https://inctagram.work/api/v1"

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { redirect } from "next/navigation";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: /*"https://inctagram.work/api/v1"*/ "https://inctagram-api.vercel.app/api/",
+  baseUrl: "https://inctagram.work/api/v1" /*"https://inctagram-api.vercel.app/api/"*/,
   prepareHeaders: (headers, { getState }) => {
     const token = sessionStorage.getItem("accessToken");
     if (token) {
