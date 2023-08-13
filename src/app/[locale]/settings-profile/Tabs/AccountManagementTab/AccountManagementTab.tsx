0d947b49dc58e9a5774b6957ffae5a38d57a81be
warning11223 <warning11223@gmail.com>
2023-08-13 15:09:17 +0300

    fix: AccountManagementTab.tsx

--- src/app/[locale]/settings-profile/Tabs/AccountManagementTab/AccountManagementTab.tsx
+++ src/app/[locale]/settings-profile/Tabs/AccountManagementTab/AccountManagementTab.tsx
@@ -1,3 +1,50 @@
+import React, { useState } from "react";
+import * as Tabs from "@radix-ui/react-tabs";
+
+import s from "./AccountManagementTab.module.scss";
+import { AccountTypeRadio } from "./AccountTypeRadio/AccountTypeRadio";
+import { SubscriptionRadio } from "./SubscriptionRadio/SubscriptionRadio";
+import Image from "next/image";
+import { Subscription } from "./Subscription/Subscription";
+
+export const AccountManagementTab = () => {
+  const [accountTypeValue, setAccountTypeValue] = useState("personal");
+  const [subTypeValue, setSubTypeValue] = useState("10");
+
+  return (
+    <Tabs.Content className={s.TabsContent} value="accountManagement">
+      <div className={s.tab}>
+        <Subscription />
+
+        <p className={s.tab__name}>Account type:</p>
+        <div className={s.tab__wrapper}>
+          <AccountTypeRadio radioValue={accountTypeValue} setRadioValue={setAccountTypeValue} />
+        </div>
+        {accountTypeValue === "business" && (
+          <>
+            <p className={s.tab__name}>Your subscription costs:</p>
+            <div className={s.tab__wrapper}>
+              <SubscriptionRadio subTypeValue={subTypeValue} setSubTypeValue={setSubTypeValue} />
+            </div>
+            <div className={s.tab__container}>
+              <div className={s.tab__img__wrapper}>
+                <Image className={s.tab__img} src={"/img/paypal.png"} alt={"paypal"} width={70} height={47} />
+              </div>
+              <p>or</p>
+              <div className={s.tab__img__wrapper}>
+                <Image className={s.tab__img} src={"/img/stripe.png"} alt={"paypal"} width={70} height={30} />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </Tabs.Content>
+  );
+};
+
+/*
+----------stripe code-----------------
+
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 
@@ -56,3 +103,4 @@ export const AccountManagementTab = () => {
     </form>
   );
 };
+*/
