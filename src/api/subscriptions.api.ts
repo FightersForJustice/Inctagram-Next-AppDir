7adf8804c242083983d655e71cd10b52b4dd8414
warning11223 <warning11223@gmail.com>
2023-08-15 23:23:54 +0300

    feat: subscriptions.api.ts

---
+++ src/api/subscriptions.api.ts
@@ -0,0 +1,90 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithReauth } from "../helpers/config";
+
+export const subscriptionsApi = createApi({
+  reducerPath: "subscriptionsApi",
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({
+    getPayments: builder.query<GetPaymentsResponse[], void>({
+      query: () => {
+        return {
+          url: "subscriptions/my-payments",
+          method: "GET",
+        };
+      },
+    }),
+    getCurrentSubscription: builder.query<GetCurrentSubscription, void>({
+      query: () => {
+        return {
+          url: "subscriptions/current-subscriptions",
+          method: "GET",
+        };
+      },
+    }),
+    getCostOfSubscription: builder.query<GetCostOfSubscription, void>({
+      query: () => {
+        return {
+          url: "subscriptions/cost-of-subscriptions",
+          method: "GET",
+        };
+      },
+    }),
+    createSubscription: builder.mutation<any, CreateSubscription>({
+      query: (arg: CreateSubscription) => {
+        return {
+          url: "subscriptions",
+          method: "POST",
+          body: arg, //maybe mistake
+        };
+      },
+    }),
+    cancelAutoRenewal: builder.mutation<void, void>({
+      query: () => {
+        return {
+          url: "subscriptions/canceled-auto-renewal",
+          method: "POST",
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetPaymentsQuery } = subscriptionsApi;
+
+type CreateSubscription = {
+  typeSubscription: string;
+  paymentType: string;
+  amount: number;
+};
+
+export type GetCostOfSubscription = {
+  data: [
+    {
+      amount: number;
+      typeDescription: string;
+    },
+  ];
+};
+
+export type GetCurrentSubscription = {
+  data: Subscription[];
+  hasAutoRenewal: boolean;
+};
+
+type Subscription = {
+  userId: number;
+  subscriptionId: string;
+  dateOfPayment: string;
+  endDateOfSubscription: string;
+  autoRenewal: boolean;
+};
+
+export type GetPaymentsResponse = {
+  userId: number;
+  subscriptionId: string;
+  dateOfPayment: string;
+  endDateOfSubscription: string;
+  price: number;
+  subscriptionType: string;
+  paymentType: string;
+};
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,15 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "api/auth.api";
 import { profileApi } from "../api/profile.api";
 import { postsApi } from "../api/posts.api";
+import { subscriptionsApi } from "../api/subscriptions.api";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [profileApi.reducerPath]: profileApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
+    [subscriptionsApi.reducerPath]: subscriptionsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([authApi.middleware, profileApi.middleware, postsApi.middleware]),
+    getDefaultMiddleware().concat([
+      authApi.middleware,
+      profileApi.middleware,
+      postsApi.middleware,
+      subscriptionsApi.middleware,
+    ]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
