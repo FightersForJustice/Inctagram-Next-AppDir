f60f76d2b3d771b63a99174f7b5ea2fea902bc4f
FelixT <felixtekhada@gmail.com>
2023-08-16 19:12:50 +0200

    added check for spaces in the password field

--- src/features/schemas/SignInSchema.ts
+++ src/features/schemas/SignInSchema.ts
@@ -1,16 +1,25 @@
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 import * as yup from "yup";
 
-
-export const SignInSchema = ()=> {
-
-const t = useTranslations("Errors")
+export const SignInSchema = () => {
+  const t = useTranslations("Errors");
 
   return yup
-  .object({
-    email: yup.string().email(t("email.email")).required(t("email.required")),
-    password: yup.string().min(6, t("password.min")).max(20, t("password.max")).required(t("password.required")),
-  })
-  .required();
-}
-
+    .object({
+      email: yup.string().email(t("email.email")).required(t("email.required")),
+      password: yup
+        .string()
+        .min(6, t("password.min"))
+        .max(20, t("password.max"))
+        .required(t("password.required"))
+        .test("not-only-spaces", t("password.onlySpaces"), (value) => {
+          // Проверяем, что пароль не состоит только из пробелов
+          return value.trim() !== "";
+        })
+        .test("no-inner-spaces", t("password.innerSpaces"), (value) => {
+          // Проверяем, что пароль не содержит пробелов внутри
+          return !/\s/.test(value);
+        }),
+    })
+    .required();
+};
