8e6f6bffcbe61952ba38a83ee8118e849be85564
FelixT <felixtekhada@gmail.com>
2023-08-19 00:46:21 +0200

    turned off hook on 404page

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -2,16 +2,18 @@
 
 import NotFound from "components/NotFound/NotFound";
 import { BaseLayout } from "components/layouts/BaseLayout/BaseLayout";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 
 export default function CatchAllPage() {
-  const t = useTranslations("NotFoundPage");
+  // const t = useTranslations("NotFoundPage");
 
   return (
 
       <NotFound>
-        <>{t("title")}</>
-        <>{t("description")}</>
+        {/* <>{t("title")}</>
+        <>{t("description")}</> */}
+        <div>Hello </div>
+        <div>world</div>
       </NotFound>
 
   );
