f89808cffb49815b62077eeaa3639d4cbdd5a673
FelixT <felixtekhada@gmail.com>
2023-08-25 15:29:34 +0200

    made thefn ln c fields optional

--- src/features/schemas/SettingsFormSchema.ts
+++ src/features/schemas/SettingsFormSchema.ts
@@ -3,9 +3,9 @@ import * as yup from "yup";
 export const SettingsFormSchema = yup
   .object({
     userName: yup.string().min(6).max(30).required(),
-    firstName: yup.string().min(5).max(15).required(),
-    lastName: yup.string().min(5).max(15).required(),
-    city: yup.string().min(3).max(15).required(),
-    aboutMe: yup.string().min(10).max(200),
+    firstName: yup.string().min(5).max(15).nullable(),
+    lastName: yup.string().min(5).max(15).nullable(),
+    city: yup.string().min(3).max(15).nullable(),
+    aboutMe: yup.string().min(10).max(200).required(),
   })
   .required();
