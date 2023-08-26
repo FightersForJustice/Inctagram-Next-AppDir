199a384c30b69aec811081299507708f632b52db
FelixT <felixtekhada@gmail.com>
2023-08-26 23:31:34 +0200

    fixed merge

--- src/features/schemas/SignUpFormSchema.ts
+++ src/features/schemas/SignUpFormSchema.ts
@@ -34,7 +34,7 @@ export const SignUpFormSchema = () => {
               return (
                 emailDomainRegex.test(subdomain) &&
                 emailDomainRegex.test(domainParts[domainParts.length - 1]) &&
-                fullDomain.includes(".") &&
+                fullDomain.includes(".") && //??
                 !local.includes("..") &&
                 !fullDomain.includes("..") &&
                 subdomain.trim() !== "" &&
@@ -55,7 +55,7 @@ export const SignUpFormSchema = () => {
       password: yup
         .string()
         .required(t("password.required"))
-        .test("not-spaces", t("password.spaces"), (value) => {
+        .test("not-spaces", t("password.spaces"), (value) => {      
           return value.trim() !== "" && !/\s/.test(value);
         })
         .min(6, t("password.min"))
