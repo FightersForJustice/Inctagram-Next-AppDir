408a50070c75381d5f67ac2715c2584418eb5ba0
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2023-11-12 19:34:47 +0100

    fixBug: add more than 10 images in post

--- src/components/ImagesCollection/ImagesCollection.tsx
+++ src/components/ImagesCollection/ImagesCollection.tsx
@@ -14,7 +14,6 @@ type Props = {
 
 export const ImagesCollection: React.FC<Props> = ({
   loadedImages,
-  setLoadedImages,
   setPostImage,
 }) => {
   const dispatch = useAppDispatch();
@@ -25,6 +24,7 @@ export const ImagesCollection: React.FC<Props> = ({
     }
   }, [loadedImages.length]);
 
+  const moreThen10Img = loadedImages.length >= 10
   const onDeleteImageFromCollection = (id: string) => {
     if (loadedImages.length === 1) {
       toast.error("Your can't delete one image");
@@ -36,12 +36,13 @@ export const ImagesCollection: React.FC<Props> = ({
   return (
     <div className={s.collection__container}>
       <Image
-        src={'/img/create-post/plus.svg'}
+        src={moreThen10Img ?'/img/create-post/plus-disable.svg' :  '/img/create-post/plus.svg'}
         alt={'plus'}
         height={36}
         width={36}
         className={s.collection__plusBtn}
-        onClick={() => setPostImage('')}
+        onClick={() => !moreThen10Img && setPostImage('')}
+
       />
       <div className={s.collection__items}>
         {loadedImages.map((item, index) => {
