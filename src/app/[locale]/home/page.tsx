510daed4d4ef34c1c6e588916d9af4029c654e44
Alexey Yankov <yankovav.wm@gmail.com>
2023-11-15 19:49:37 +0300

    home

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { SideBar } from '../my-profile/Navigation';
+import { SideBar } from '../my-profile/Navigation/SideBar';
 import { usePathname } from 'next-intl/client';
 import { toast } from 'react-toastify';
 import { Loader } from '@/components/Loader';
