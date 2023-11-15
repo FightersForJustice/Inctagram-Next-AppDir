5f3f4e9c52ea175e10db6b34a7f04df1ddd874d6
Alexey Yankov <yankovav.wm@gmail.com>
2023-11-15 16:17:37 +0300

    vercel 1.0

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useEffect, useState } from 'react';
 import { SideBar } from '../my-profile/Navigation/SideBar';
 import { usePathname } from 'next-intl/client';
--- src/app/[locale]/my-profile/page.tsx
+++ src/app/[locale]/my-profile/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useState } from 'react';
 import s from './MyProfile.module.scss';
 import { usePathname } from 'next-intl/client';
