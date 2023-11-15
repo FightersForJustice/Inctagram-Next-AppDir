444f45673300d40d0683626c5111d9535342e5dc
Alexey Yankov <yankovav.wm@gmail.com>
2023-11-15 19:40:18 +0300

    fix export

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { SideBar } from '../my-profile/Navigation/SideBar';
+import { SideBar } from '../my-profile/Navigation';
 import { usePathname } from 'next-intl/client';
 import { toast } from 'react-toastify';
 import { Loader } from '@/components/Loader';
@@ -13,7 +13,7 @@ import s from './Home.module.scss';
 import { StatusCode } from '@/api/auth.api';
 
 const Home: React.FC = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
   const [posts, setPosts] = useState<PostsItem[]>([]);
   const [fetching, setFetching] = useState<boolean>(false);
   const [lastLoadedPostId, setLastLoadedPostId] = useState<number>(0);
@@ -84,6 +84,7 @@ const Home: React.FC = () => {
     <>
       <div className={s.container}>
         <div className={s.wrapper} id={'wrapper'}>
+          {/*!paidAccount where get?*/}
           <SideBar pathname={pathname} paidAccount={false} />
           <div style={{ gridArea: 'profile' }}>{allPosts}</div>
         </div>
