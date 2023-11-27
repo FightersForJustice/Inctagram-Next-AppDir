5bebbfac305891c1b1c7f3803a2a608cfcae6e1d
TURP1 <turpicrypto@gmail.com>
2023-11-27 02:12:49 +0200

    bugfix/vp-212/validation-password-recovery

--- src/features/schemas/CreateNewPasswordFormSchema.ts
+++ src/features/schemas/CreateNewPasswordFormSchema.ts
@@ -4,25 +4,20 @@ import * as yup from 'yup';
 export const CreateNewPasswordFormSchema = () => {
   const t = useTranslations('Errors');
 
-  const passwordValidationRegex =
-    /^[A-Za-z0-9!"@#$%^&*'()_+{}\[\]:;<>,.?~\-=/\\|]+$/;
+  const passwordCompletly =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\-=/\\|])[A-Za-z0-9'"`!@#$%^&*()_+{}\[\]:;<>,.?~\-=/\\|]{6,20}$/;
 
   return yup
     .object({
       password: yup
         .string()
         .required(t('password.required'))
-        .test('not-only-spaces', t('password.spaces'), (value) => {
-          // Проверяем, что пароль не состоит только из пробелов
-          return value.trim() !== '';
+        .test('not-spaces', t('password.spaces'), (value) => {
+          return value.trim() !== '' && !/\s/.test(value);
         })
-        .test('no-inner-spaces', t('password.spaces'), (value) => {
-          // Проверяем, что пароль не содержит пробелов внутри
-          return !/\s/.test(value);
-        })
-        .matches(passwordValidationRegex, t('password.invalidCharacters'))
         .min(6, t('password.min'))
-        .max(20, t('password.max')),
+        .max(20, t('password.max'))
+        .matches(passwordCompletly, `${t('password.complexity')} `),
       passwordConfirm: yup
         .string()
         .oneOf([yup.ref('password')], t('passwordConfirm.oneOf'))
@@ -30,5 +25,4 @@ export const CreateNewPasswordFormSchema = () => {
         .required(t('passwordConfirm.required')),
     })
 
-    .required();
 };
