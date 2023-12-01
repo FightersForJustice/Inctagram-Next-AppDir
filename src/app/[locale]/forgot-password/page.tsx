d34ae87c6b3fcf0ee71e215666bd0890f2ba08c9
victoriapavlova <victoria.pavlova7@gmail.com>
2023-12-01 13:28:21 +0300

    refactor styles for forgot password, 3/7 pages from figma

--- src/app/[locale]/forgot-password/page.tsx
+++ src/app/[locale]/forgot-password/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import s from './Forgot-Password.module.scss';
 import { useTranslations } from 'next-intl';
 import { ForgotPasswordForm } from './ForgotPasswordForm';
 
@@ -8,13 +9,8 @@ const ForgotPassword = () => {
   const t = useTranslations('ForgotPasswordPage');
 
   return (
-    <div
-      className={
-        'border-solid border-1 border-[--dark-300] bg-[--dark-500] rounded-md m-auto mt-[100px] max-w-[378px] text-center'
-      }
-    >
-      <p className={'font-bold text-xl pt-[23px]'}>{t('title')}</p>
-      <div className={'flex gap-[60px] justify-center mt-[13px]'}></div>
+    <div className={s.container}>
+      <p className={s.title}>{t('title')}</p>
       <ForgotPasswordForm translate={t} />
     </div>
   );
