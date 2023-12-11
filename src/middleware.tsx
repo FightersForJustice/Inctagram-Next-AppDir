f7820c7a8cc69d7f01f9f6884849cbdf1c93ea7c
maskpa100 <maskpa@yandex.ru>
2023-12-12 02:58:14 +0800

    refactor

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,5 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getServerAuth } from './utils/auth/getServerAuth';
 
 export default createMiddleware({
@@ -11,3 +11,21 @@ export const config = {
   // Skip all paths that should not be internationalized
   matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
+
+export async function middleware(request: NextRequest) {
+  console.log(request.cookies.get('accessToken')?.value);
+  const requestOptions: RequestInit = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${request.cookies.get('accessToken')?.value}`,
+    },
+  };
+
+  let url = 'https://inctagram.work/api/v1/auth/me';
+  let response = await fetch(url, requestOptions);
+  console.log('response status ' + response.status);
+  let commits = await response.json();
+  console.log(commits);
+
+  // return NextResponse.rewrite(new URL('/ru/sign-up', request.url));
+}
