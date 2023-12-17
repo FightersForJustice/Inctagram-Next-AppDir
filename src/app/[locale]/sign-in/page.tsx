0452772a2fefb97f33290a7d5c44ecc486c1cf7e
maskpa100 <maskpa@yandex.ru>
2023-12-17 19:29:29 +0800

    refactor

--- src/app/[locale]/sign-in/page.tsx
+++ src/app/[locale]/sign-in/page.tsx
@@ -1,11 +1,22 @@
-import SignInPageClient from './SignInPageClient';
+'use client';
 
-const SignInPage = () => {
+import React from 'react';
+import s from './SignIn.module.scss';
+import { SignIn } from './SignIn';
+import { useTranslations } from 'next-intl';
+import ServiceAuth from './SignIn/ServiceAuth';
+
+const SignInPageClient = () => {
+  const t = useTranslations('SignInPage');
+
   return (
     <>
-      <SignInPageClient />
+      <div id={'sign-in'} className={s.container}>
+        <ServiceAuth page={'SignInPage'} />
+        <SignIn translate={t} />
+      </div>
     </>
   );
 };
 
-export default SignInPage;
+export default SignInPageClient;
