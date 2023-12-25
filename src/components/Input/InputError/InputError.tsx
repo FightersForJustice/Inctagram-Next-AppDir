ad96e652b9de77a80333480e46cd0acce7319c70
Alexey Yankov <yankovav.wm@gmail.com>
2023-12-25 20:10:11 +0300

    fix errors style

--- src/components/Input/InputError/InputError.tsx
+++ src/components/Input/InputError/InputError.tsx
@@ -1,26 +1,12 @@
 import { InputErrorProps } from '@/types/signUpTypes';
-import s from './InputError.module.scss';
-import clsx from 'clsx';
 import { dictionary } from '@/features/data/passwordSymbols';
+import { errorStyle, useStyle } from './utils';
 
 export const InputError = ({ errorMessage, error, id }: InputErrorProps) => {
-  const signUpStyle = clsx(
-    s.container,
-    {
-      [s.containerSignUp]: id === 'sign-up-passwordConfirm',
-    },
-    {
-      [s.containerSignUpBottom]: id === 'sign-up-password',
-    },
-    {
-      [s.containerSignUpCheckbox]: id === 'sign-up-agreements',
-    }
-  );
   return (
     error && (
-      <p className={signUpStyle} id={id}>
-        {errorMessage}{' '}
-        {error.message === 'Password must contain 0-9, a-z, A-Z ' && dictionary}
+      <p className={useStyle(id, error.toString(), id.slice(0, 7) === 'sign-in')} id={id}>
+        {errorMessage} {errorStyle(error.message) && dictionary}
       </p>
     )
   );
