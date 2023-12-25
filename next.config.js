2815a2fa4852e9adb231e550439a5c8a634a4015
Dmitriy <explay167@gmail.com>
2023-12-25 14:19:20 +0300

    Preparation

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     NEXT_PUBLIC_RECAPTCHA_SECRET_KEY:
       process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY,
   },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ru'],
+  },
 };
 
 module.exports = nextConfig;
