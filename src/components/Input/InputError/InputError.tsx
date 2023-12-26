923ffe9c3874a90dd317fc08c8e35fa3a3968330
Alexey Yankov <yankovav.wm@gmail.com>
2023-12-26 12:24:35 +0300

    fix vercel

--- src/components/Input/InputError/InputError.tsx
+++ src/components/Input/InputError/InputError.tsx
@@ -22,7 +22,7 @@ export const InputError = ({ errorMessage, error, id }: InputErrorProps) => {
   return (
     error && (
       <p
-        className={useStyle(id, error.toString(), id.slice(0, 7) === 'sign-in')}
+        className={setStyle(id, error.toString(), id.slice(0, 7) === 'sign-in')}
         id={id}
       >
         {errorMessage} {errorStyle(error.message) && dictionary}
