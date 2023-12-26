786b774d5b765d19b3d4d2ecabe6e369a8771a55
TURP1 <turpicrypto@gmail.com>
2023-12-26 13:51:52 +0200

    format

--- src/features/customHooks/useLogout.ts
+++ src/features/customHooks/useLogout.ts
@@ -1,22 +1,25 @@
-import Cookies from "js-cookie";
-import { Dispatch, SetStateAction } from "react"
-import { toast } from "react-toastify";
+import Cookies from 'js-cookie';
+import { Dispatch, SetStateAction } from 'react';
+import { toast } from 'react-toastify';
 
-import { logOutAction } from "@/app/actions";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import { logOutAction } from '@/app/actions';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 
+export const useLogout = async (
+  setShowLogoutModal: Dispatch<SetStateAction<boolean>>,
+  t: any,
+  router: AppRouterInstance
+) => {
+  setShowLogoutModal(false);
+  const refreshToken = Cookies.get('refreshToken');
 
-export const useLogout = async (setShowLogoutModal: Dispatch<SetStateAction<boolean>>, t: any, router: AppRouterInstance) => {
-    setShowLogoutModal(false);
-    const refreshToken = Cookies.get('refreshToken');
+  const res = await logOutAction(refreshToken);
 
-    const res = await logOutAction(refreshToken);
+  if (res?.success) {
+    Cookies.remove('refreshToken');
+    Cookies.remove('accessToken');
 
-    if (res?.success) {
-        Cookies.remove('refreshToken');
-        Cookies.remove('accessToken');
-
-        router.push('/sign-in');
-        toast.success(t(res.data));
-    } else toast.error(t(res?.error));
-}
+    router.push('/sign-in');
+    toast.success(t(res.data));
+  } else toast.error(t(res?.error));
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export function getUserPreferredLanguage(acceptLanguage: string | null) {
     if (preferredLanguage?.startsWith('ru')) {
       return 'ru';
     }
-  } catch { }
+  } catch {}
   return 'en';
 }
 
