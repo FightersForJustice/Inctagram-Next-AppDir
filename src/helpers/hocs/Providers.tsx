326b98d5f5d39529301226df255c07ea591bb7f6
Dmitriy <explay167@gmail.com>
2023-12-27 17:15:16 +0300

    add </I18nextProvider>

--- src/helpers/hocs/Providers.tsx
+++ src/helpers/hocs/Providers.tsx
@@ -4,14 +4,36 @@ import { Provider } from 'react-redux';
 import { store } from '../../redux/store';
 import React from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import i18n from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import en from '../../../public/locales/en/en.json';
+import ru from '../../../public/locales/ru/ru.json';
+
+i18n.use(initReactI18next).init({
+  detection: {
+    order: ['localStorage', 'htmlTag', 'path', 'subdomain'],
+    caches: ['localStorage'],
+  },
+  lng: 'en',
+  resources: {
+    en: {
+      translation: en,
+    },
+    ru: {
+      translation: ru,
+    },
+  },
+});
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <GoogleOAuthProvider
-      clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''}
-    >
-      <Provider store={store}>{children}</Provider>
-    </GoogleOAuthProvider>
+    // <I18nextProvider i18n={i18n}>
+      <GoogleOAuthProvider
+        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''}
+      >
+        <Provider store={store}>{children}</Provider>
+      </GoogleOAuthProvider>
+    // </I18nextProvider>
   );
 }
 export default Providers;
