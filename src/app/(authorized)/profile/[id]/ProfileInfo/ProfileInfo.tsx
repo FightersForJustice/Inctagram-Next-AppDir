f7a7f7d20b4a8f2e339eb4ee892b134f550653f3
maskpa100 <maskpa@yandex.ru>
2024-01-14 19:30:12 +0800

    refactor

--- src/app/(authorized)/profile/[id]/ProfileInfo/ProfileInfo.tsx
+++ src/app/(authorized)/profile/[id]/ProfileInfo/ProfileInfo.tsx
@@ -60,7 +60,7 @@ export const ProfileInfo = ({ userData, myProfile }: Props) => {
                 )}
               </div>
             </div>
-            <div className={s.descriptions}>
+            <div className={myProfile ? s.descriptions : s.descriptionsPublic}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
