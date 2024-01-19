6654ca8ec8298e544198d953b3f51481e7a063b8
victoriapavlova <victoria.pavlova7@gmail.com>
2024-01-19 20:55:58 +0300

    email expired fixed

--- src/app/(not_authorized)/email-expired/page.tsx
+++ src/app/(not_authorized)/email-expired/page.tsx
@@ -4,10 +4,9 @@ import { useTranslation } from 'react-i18next';
 import { RegistrationEmailResend } from '@/components/auth';
 
 const EmailExpired = () => {
- 
   const { t } = useTranslation();
   const translate = (key: string): string => t(`EmailExpiredPage.${key}`);
-  return <RegistrationEmailResend translate={t} />;
+  return <RegistrationEmailResend translate={translate} />;
 };
 
 export default EmailExpired;
