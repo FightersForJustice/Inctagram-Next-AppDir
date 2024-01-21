fea61076a055cbd3f1659d5ce7095115d9cceaea
TURP1 <turpicrypto@gmail.com>
2024-01-21 20:54:34 +0200

    fix scrollbar & design

--- src/components/Buttons/PrimaryBtn/PrimaryBtn.tsx
+++ src/components/Buttons/PrimaryBtn/PrimaryBtn.tsx
@@ -1,18 +1,27 @@
 import React, { MouseEventHandler, ReactNode } from 'react';
 import s from './PrimaryBtn.module.scss';
+import clsx from 'clsx';
 
 type Props = {
   type?: 'submit';
   children: ReactNode;
   disabled?: boolean;
+  className?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const PrimaryBtn = ({ children, disabled, onClick, type }: Props) => {
+export const PrimaryBtn = ({
+  children,
+  disabled,
+  onClick,
+  type,
+  className,
+}: Props) => {
+  const classNames = clsx(s.primaryBtn, className);
   return (
     <button
       type={type}
-      className={s.primaryBtn}
+      className={classNames}
       disabled={disabled}
       onClick={onClick}
     >
