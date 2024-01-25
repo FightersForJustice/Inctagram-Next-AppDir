2ea75ccbd7c61de16b5ef52139dbe63ca79ba44a
Alexey Yankov <yankovav.wm@gmail.com>
2024-01-25 11:12:13 +0300

    final small sizes switcher

--- src/components/Header/HeaderTranslation/LanguagesModal.tsx
+++ src/components/Header/HeaderTranslation/LanguagesModal.tsx
@@ -7,30 +7,42 @@ export const LanguagesModal = function ({
   closeModal,
   language,
   onSelectChange,
+  isEn,
 }: {
   closeModal: () => void;
   onSelectChange: (value: string) => void;
   language: string;
+  isEn: boolean;
 }) {
-  const langHandler = (lang: string) => {
-    closeModal();
-    if (lang === 'ru') {
-      return onSelectChange('en');
+  const langHandler = (lang: string, e: React.MouseEvent) => {
+    if (window.screen.width > 521 || e.currentTarget.id === 'isMobile') {
+      console.log(e.currentTarget.id, lang)
+      closeModal();
+      if (lang === 'ru') {
+        return onSelectChange('en');
+      }
+      onSelectChange('ru');
     }
-    onSelectChange('ru');
   };
+  console.log(!isEn);
+
+  const finalLang = window.screen.width < 521 ? '/img/flag_russia.svg' : '/img/flag_united_kingdom.svg'
+  const isMobileLanguage = window.screen.width < 521 && !isEn ? f.active : '';
   const languageForRender = (lang: string) => {
     return (
       <div
-        className={f.container + ' ' + f.languagesContainer}
-        onClick={() => langHandler(lang)}
+        className={
+          f.container + ' ' + f.languagesContainer + ' ' + isMobileLanguage
+        }
+        id='isMobile'
+        onClick={(e) => langHandler(lang, e)}
       >
         <Image
           alt="no-image"
           src={
             lang === 'en'
               ? '/img/flag_russia.svg'
-              : '/img/flag_united_kingdom.svg'
+              :  finalLang
           }
           width={20}
           height={20}
--- src/components/Header/HeaderTranslation/TranslationSelect.tsx
+++ src/components/Header/HeaderTranslation/TranslationSelect.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import Image from 'next/image';
 import { LanguagesModal } from './LanguagesModal';
@@ -12,39 +12,68 @@ import f from './HeaderTranslation.module.scss';
 export const TranslationSelect = () => {
   const { i18n } = useTranslation();
   const [openChangeSize, setOpenChangeSize] = useState(false);
+  const [isEn, setEn] = useState(() => {
+    const storedLanguage = localStorage.getItem('language');
+    return storedLanguage === 'en' || false});
   const [language, setLanguage] = useState(() => {
     const storedLanguage = localStorage.getItem('language');
     return storedLanguage || 'en';
   });
 
   const onSelectChange = (value: string) => {
-    setLanguage(value);
-    i18n.changeLanguage(value);
-    localStorage.setItem('language', String(value));
+    if (window.screen.width > 521) {
+      console.log('is shit!1');
+      setLanguage(value);
+      i18n.changeLanguage(value);
+      localStorage.setItem('language', String(value));
+      return;
+    }
+  };
+
+  const mobileHandler = (value: boolean) => {
+    if (window.screen.width < 521 && !isEn && value) {
+      setEn(true);
+      console.log(9)
+      i18n.changeLanguage('en');
+      localStorage.setItem('language', String('en'));
+      return 
+    }
+    if (window.screen.width < 521 && isEn && !value) {
+      setEn(false);
+      i18n.changeLanguage('ru');
+      return localStorage.setItem('language', String('ru'));
+    }
+    
   };
 
   const fullLanguages: any = {
     ru: { name: 'Русский', img: '/img/flag_russia.svg' },
     en: { name: 'English', img: '/img/flag_united_kingdom.svg' },
   };
+  const isOpen = window.screen.width < 768 ? true : false
+
+  const finalImage = window.screen.width < 768 ? fullLanguages['en']['img'] : fullLanguages[language]['img']
+
+  const isMobileLanguage = isEn && window.screen.width < 768 ? f.active : '';
 
   return (
     <Popover.Root
-      open={openChangeSize}
+      open={isOpen}
       onOpenChange={() => setOpenChangeSize(!openChangeSize)}
     >
-      <div className={s.cropping__wrapper}>
-        <Popover.Trigger className={f.container}>
+      <div className={s.cropping__wrapper + ' ' + isMobileLanguage} >
+        <Popover.Trigger className={f.container} onClick={()=>mobileHandler(true)}>
           <div className={f.select}>
             <Image
               alt="flag-image"
-              src={fullLanguages[language]['img']}
+              src={finalImage}
               width={20}
               height={20}
             />
             <span>{fullLanguages[language]['name']}</span>
           </div>
           <Image
+            className={f.translate_select}
             alt="arrow-image"
             src="/img/arrowDown-light.svg"
             width={20}
@@ -52,11 +81,13 @@ export const TranslationSelect = () => {
           />
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className={f.popOver}>
+          <Popover.Content className={f.popOver} onClick={()=>mobileHandler(false)}>
             <LanguagesModal
               closeModal={() => setOpenChangeSize(false)}
               onSelectChange={onSelectChange}
               language={language}
+              isEn={isEn}
+              
             />
           </Popover.Content>
         </Popover.Portal>
