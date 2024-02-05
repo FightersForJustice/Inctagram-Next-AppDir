8ca2ea0da5433bf085c6442e2e22ece27ccf0eb7
TURP1 <turpicrypto@gmail.com>
2024-02-05 11:01:14 +0200

    fix Base Layout privacy

--- src/components/layouts/BaseLayout/BaseLayout.tsx
+++ src/components/layouts/BaseLayout/BaseLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { Header } from '@/components/Header';
+import styles from './BaseLayout.module.scss';
 
 type Props = {
   title?: string;
@@ -10,7 +11,9 @@ export const BaseLayout = ({ children }: Props) => {
   return (
     <div>
       <Header isAuth={false} />
-      {children}
+      <main className={styles.contentContainer}>
+        <div className={styles.content}>{children}</div>
+      </main>
     </div>
   );
 };
