99ab97c00d8a7492ef00300e489fe1602587e45f
maskpa100 <maskpa@yandex.ru>
2024-03-18 00:00:53 +0800

    r

--- src/components/ProfileSettings/Tabs/DevicesTab/utils/findDevice.ts
+++ src/components/ProfileSettings/Tabs/DevicesTab/utils/findDevice.ts
@@ -1,9 +1,11 @@
 export function findDevice(sessions: any, userAgentArray: any) {
-  return sessions.find(
-    (item: any) =>
-      item.browserName === userAgentArray.browser.name &&
-      item.browserVersion === userAgentArray.browser.version &&
-      item.osName === userAgentArray.os.name &&
-      item.osVersion === userAgentArray.os.version
-  );
+  if (sessions) {
+    return sessions.find(
+      (item: any) =>
+        item.browserName === userAgentArray.browser.name &&
+        item.browserVersion === userAgentArray.browser.version &&
+        item.osName === userAgentArray.os.name &&
+        item.osVersion === userAgentArray.os.version
+    );
+  }
 }
