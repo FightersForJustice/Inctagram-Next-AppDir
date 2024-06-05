a17452eba1cbb9017ebacd6c6db1472f9dd0b608
Pasha <klonirovan89@gmail.com>
2024-06-05 17:40:24 +0300

    deleted headers() and accessToken (unused here)

--- src/app/(authorized)/profile/[id]/ProfileServer/ProfileServer.tsx
+++ src/app/(authorized)/profile/[id]/ProfileServer/ProfileServer.tsx
@@ -4,17 +4,14 @@ import { ProfileInfo } from '../ProfileInfo/ProfileInfo';
 import s from './ProfileServer.module.scss';
 import { Posts } from '../Posts/Posts';
 
-import { headers } from 'next/headers';
-
 import { ApiResponsePosts } from '@/redux/reducers/MyProfile/ProfilePostReducer';
 
 type Props = {
   id: number;
   myProfile: boolean;
 };
+
 const ProfileServer = async ({ id, myProfile }: Props) => {
-  const headersList = headers();
-  const accessToken = headersList.get('accessToken');
   const userdata: UserProfile = await getProfile(id);
   //await new Promise((resolve) => setTimeout(resolve, 3000));
   const postsData: ApiResponsePosts = await getPosts(id, 0);
