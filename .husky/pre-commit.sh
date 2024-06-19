ab67a8ad014a07c23cfe114ec9b1fb6575d39039
Alexey Yankov <yankovav.wm@gmail.com>
2024-06-19 21:00:15 +0300

    check

--- .husky/pre-commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
-
-# Custom script to add a tag
-TAG_NAME="v$(date +%Y%m%d%H%M%S)"
-git tag $TAG_NAME
-git push origin $TAG_NAME
--- src/accessToken.ts
+++ src/accessToken.ts
@@ -3,4 +3,4 @@ export function setAccessToken(str: string) {
   accessToken = str;
 }
 
-console.log('test11')
+console.log('test7')
