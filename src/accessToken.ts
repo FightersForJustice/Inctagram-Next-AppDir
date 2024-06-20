2870098f02c579098c7bb47e40b4c11f8ad7786b
Alexey Yankov <yankovav.wm@gmail.com>
2024-06-20 12:02:55 +0300

    fgfgfg

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -3,4 +3,4 @@ export function setAccessToken(str: string) {
   accessToken = str;
 }
 
-console.log('dftt22RRRqqq11tryeee2ee4488877770000884130031tes7414t3429')
+console.log('yyyydftt22RRRqqq11tryeee2ee4488877770000884130031tes7414t3429')
