5802d8500eab0ce54e76efd96ad30cd368827b9e
Alexey Yankov <yankovav.wm@gmail.com>
2024-06-20 12:50:36 +0300

    feat(pill): message here7

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -3,4 +3,4 @@ export function setAccessToken(str: string) {
   accessToken = str;
 }
 
-console.log('ssssss666sssssffdasafff4888777es7414t3429');
+console.log('vvvv333ssssss666sssssffdasafff4888777es7414t3429');
