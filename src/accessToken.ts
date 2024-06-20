49576153c194dfa63678cb9351630863836959c8
Alexey Yankov <yankovav.wm@gmail.com>
2024-06-20 12:01:41 +0300

    tryewee

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -3,4 +3,4 @@ export function setAccessToken(str: string) {
   accessToken = str;
 }
 
-console.log('tryeee2ee4488877770000884130031tes7414t3429')
+console.log('11tryeee2ee4488877770000884130031tes7414t3429')
