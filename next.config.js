e5bd97d9f0ecbd148b769d729b78aab5d7d2b482
Alexey Yankov <yankovav.wm@gmail.com>
2024-06-28 14:03:42 +0300

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
 const nextConfig = {
   images: {
     domains: [
@@ -15,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
