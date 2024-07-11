72c87c3381ffab21b18a2d23a8959ff1dcfb955f
Pasha <klonirovan89@gmail.com>
2024-07-11 13:33:46 +0300

    added PublicPost component

---
+++ src/app/(not_authorized)/(public-info)/public-post-page/[id]/Posts/PublicPost/PublicPost.tsx
@@ -0,0 +1,52 @@
+'use client';
+import Image from 'next/image';
+
+import s from './PublicPost.module.scss';
+import k from '../../PostFix/PostContent/PostContent.module.scss';
+import { useGetLanguage } from '@/redux/hooks/useGetLanguage';
+import { useTranslation } from 'react-i18next';
+import { getTimeAgoText } from '@/utils';
+import { PostType, UserProfile } from '../../types';
+import Link from 'next/link';
+import { AUTH_ROUTES } from '@/appRoutes/routes';
+import { Post } from '@/app/(not_authorized)/(public-info)/public-post-page/[id]/Posts/Post';
+import React from 'react';
+import {
+  ReadMoreButton,
+} from '@/components/ReadMoreButton/ReadMoreButton';
+
+type Props = {
+  post: PostType;
+  mockedProfile: UserProfile;
+};
+
+export const PublicPost = ({ post, mockedProfile }: Props) => {
+  const language = useGetLanguage();
+  const { t } = useTranslation();
+  const translate = (key: string): string => t(`Time.${key}`);
+  const time = getTimeAgoText(post.createdAt, language, translate);
+
+  return (
+    <>
+      <div className={s.postWrapper}>
+        <Post post={post} userData={mockedProfile} myProfile={false} />
+        <Link href={AUTH_ROUTES.PUBLIC_PROFILE_PAGE + `${'/' + post.ownerId}`}>
+          <div className={s.userContainer}>
+            <Image
+              src={post.avatarOwner ?? '/img/create-post/no-image.png'}
+              alt={'ava'}
+              width={36}
+              height={36}
+              className={k.post__avatar}
+            />
+            <h3 className={s.userName}>{post.userName} </h3>
+          </div>
+        </Link>
+        <p className={s.time}>{time}</p>
+      </div>
+      <div className={s.description}>
+        <ReadMoreButton text={post.description} maxLength={80}/>
+      </div>
+    </>
+  );
+};
