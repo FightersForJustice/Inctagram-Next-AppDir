f839c0ad322d6c6d715fa28221879afc1488dcbb
Pasha <klonirovan89@gmail.com>
2024-07-11 13:36:02 +0300

    added PublicPost for PublicPage, fixed style for PublicPage

--- src/app/(not_authorized)/(public-info)/public-post-page/page.tsx
+++ src/app/(not_authorized)/(public-info)/public-post-page/page.tsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import { getPublicPostsPage } from '@/app/(not_authorized)/(public-info)/public-profile/[id]/actions';
-import { Post } from '@/app/(not_authorized)/(public-info)/public-post-page/[id]/Posts/Post';
 import s from './PublicPage.module.scss';
 import Counter from '@/components/Counter/counter';
-import Link from 'next/link';
-import { AUTH_ROUTES } from '@/appRoutes/routes';
 import { UserProfile } from './[id]/types';
+import {
+  PublicPost,
+} from '@/app/(not_authorized)/(public-info)/public-post-page/[id]/Posts/PublicPost/PublicPost';
 
 const PublicPostPage = async () => {
   const publicPostPageData = await getPublicPostsPage();
@@ -15,27 +15,20 @@ const PublicPostPage = async () => {
   const postsImages = () => {
     return publicPostPageData.items.slice(0, 4).map((i: any) => {
       const mockedProfile = { id: i.ownerId } as UserProfile;
+
       return (
         <div key={i.id} className={s.postContainer}>
-          <div key={i.id}>
-            <Post post={i} userData={mockedProfile} myProfile={false} />
-          </div>
-          <Link href={AUTH_ROUTES.PUBLIC_PROFILE_PAGE + `${'/' + i.ownerId}`}>
-            <div className={s.userNameContainer}>{i.userName}</div>
-          </Link>
-          <div className={s.textContainer}>{i.description}</div>
+          <PublicPost post={i} mockedProfile={mockedProfile}/>
         </div>
       );
     });
   };
 
   return (
-    <>
+    <div className={s.wrapper}>
       <Counter />
-      <div className={s.container}>
-        <div className={s.publicImage}>{postsImages()}</div>
-      </div>
-    </>
+        <div className={s.container}>{postsImages()}</div>
+    </div>
   );
 };
 
