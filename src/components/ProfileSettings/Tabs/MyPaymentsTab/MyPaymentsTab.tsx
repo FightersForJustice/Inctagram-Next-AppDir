bf020b8f872446e52e7ff0f0d50469bb35b3432e
klonirovan89 <klonirovan89@gmail.com>
2024-08-06 23:13:26 +0300

    added PaymentsTable and PaymentsTableMobile components, fixed style for tables

--- src/components/ProfileSettings/Tabs/MyPaymentsTab/MyPaymentsTab.tsx
+++ src/components/ProfileSettings/Tabs/MyPaymentsTab/MyPaymentsTab.tsx
@@ -1,52 +1,57 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Pagination } from '@/components/newPagination/pagination';
-import { dateToFormat } from '@/utils/dateToFormat';
 
 import { PaymentsType } from '@/app/(authorized)/profile/settings-profile/types';
 import { useTranslation } from 'react-i18next';
-import s from './MyPayments.module.scss';
+import { PaymentsTable } from '@/components/ProfileSettings/Tabs/MyPaymentsTab/PaymentsTable/PaymentsTable';
+import {
+  PaymentsTableMobile
+} from '@/components/ProfileSettings/Tabs/MyPaymentsTab/PaymentsTableMobile/PaymentsTableMobile';
 
 export const MyPaymentsTab = ({ data }: { data: Array<PaymentsType> }) => {
   const { t } = useTranslation();
   const translate = (key: string): string =>
     t(`SettingsProfilePage.MyPaymentsTab.${key}`);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [paymentsPerPage, setPaymentsPerPage] = useState(5);
+  const [width, setWidth] = useState(1920);
   const lastPaymentIndex = currentPage * paymentsPerPage;
   const firstPaymentIndex = lastPaymentIndex - paymentsPerPage;
   const currentData = data.slice(firstPaymentIndex, lastPaymentIndex);
+
+  const tableHeaderData = [
+    translate('DateOfPayment'),
+    translate('EndOfPayment'),
+    translate('Price'),
+    translate('Period'),
+    translate('Type')
+  ]
 
   if (currentData.length === 0) {
     setCurrentPage(1);
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div className={s.table}>
-        <div className={s.tableHeader}>
-          <div>{translate('DateOfPayment')}</div>
-          <div>{translate('EndOfPayment')}</div>
-          <div>{translate('Price')}</div>
-          <div>{translate('Period')}</div>
-          <div>{translate('Type')}</div>
-        </div>
-        <div className={s.tableBody}>
-          {currentData.map((payment, index) => {
-            return (
-              <div key={index} className={s.tableBodyItem}>
-                <div>{dateToFormat(payment.dateOfPayment)}</div>
-                <div>{dateToFormat(payment.endDateOfSubscription)}</div>
-                <div>${payment.price}</div>
-                <div>{payment.subscriptionType}</div>
-                <div>{payment.paymentType}</div>
-              </div>
-            );
-          })
-          }
-        </div>
-      </div>
+      {width <= 521 ?
+        <PaymentsTableMobile currentData={currentData} tableHeaderData={tableHeaderData}/>
+        :
+        <PaymentsTable currentData={currentData} tableHeaderData={tableHeaderData}/>}
       <Pagination
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
