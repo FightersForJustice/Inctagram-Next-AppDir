516611daaff07935db72bc989ca5fdb2ead9bef5
Сергей <privet15.89@mail.ru>
2024-09-06 23:48:24 +0500

    feat: usersApi

---
+++ src/api/users.api.ts
@@ -0,0 +1,44 @@
+import { api } from '@/api/api';
+import { Avatar } from '@/api/profile.api';
+
+export const usersApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query<
+      GetUsers,
+      {
+        search: string;
+        pageSize: number;
+        pageNumber: number;
+        cursor: number;
+      }
+    >({
+      query: ({ search, pageSize, pageNumber, cursor }) => {
+        return {
+          url: `users?search=${search}&pageSize=${pageSize}&pageNumber=${pageNumber}&cursor=${cursor}`,
+          method: 'GET',
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersApi;
+
+export type GetUsers = {
+  totalCount: number;
+  pagesCount: number;
+  page: number;
+  pageSize: number;
+  prevCursor: number;
+  nextCursor: number;
+  items: UserType[];
+};
+
+export type UserType = {
+  id: number;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  avatars: Array<Avatar & { createdAt: string }>;
+  createdAt: string;
+};
