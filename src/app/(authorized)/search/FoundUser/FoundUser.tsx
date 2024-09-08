0f5845f55885c9a19e5c9e5d655d52a77d7863ae
Сергей <privet15.89@mail.ru>
2024-09-08 12:45:59 +0500

    feat: base logic

--- src/app/(authorized)/search/FoundUser/FoundUser.tsx
+++ src/app/(authorized)/search/FoundUser/FoundUser.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 
 import s from './FoundUser.module.scss';
+import { UserType } from '@/api/users.api';
 
-export const FoundUser = () => {
+type Props = {
+  user: UserType;
+};
+
+export const FoundUser: React.FC<Props> = ({ user }) => {
+  const { userName, firstName, lastName, id, avatars } = user;
+
   return (
     <div className={s.search__wrapper}>
       <div className={s.search__header}>
         <Image
           className={s.search__header__image}
-          src={'/img/profile/avatar.png'}
+          src={avatars[0]?.url || '/img/avatar.jpg'}
           alt={'avatar'}
           width={50}
           height={50}
         />
         <div>
-          <p className={s.search__username}>Ekat Ivanova</p>
-          <p className={s.search__name}>Ekaterina Ivanova</p>
+          <p className={s.search__username}>{userName}</p>
+          <p className={s.search__name}>
+            {firstName} {lastName}
+          </p>
         </div>
       </div>
     </div>
