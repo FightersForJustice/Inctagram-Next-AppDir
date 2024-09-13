8c841d908aab654ce18ab25e10ea26b22c3dc5c6
Сергей <privet15.89@mail.ru>
2024-09-13 17:38:38 +0500

    feat: render searched usersList

--- src/app/(authorized)/search/page.tsx
+++ src/app/(authorized)/search/page.tsx
@@ -1,21 +1,25 @@
 import { SearchContent } from './SearchContent';
 
 import s from './Search.module.scss';
-import { getUsersAction } from '@/app/(authorized)/search/SearchContent/actions';
+import { getUsers } from '@/app/(authorized)/search/SearchContent/actions';
 
 interface Params {
   searchParams: {
     search: string;
+    pageSize: number;
+    pageNumber: number;
+    cursor: number;
   };
 }
 
 const Page = async ({ searchParams }: Params) => {
-  const users = await getUsersAction(searchParams.search);
+  const { search, pageSize, pageNumber, cursor } = searchParams;
+  const { items } = await getUsers(search, pageSize, pageNumber, cursor);
 
   return (
     <div className={s.container}>
       <div className={s.wrapper} id={'wrapper'}>
-        <SearchContent users={users.items} />
+        <SearchContent users={items} />
       </div>
     </div>
   );
