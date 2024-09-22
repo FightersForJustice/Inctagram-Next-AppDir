55209f6da307ff34d42c62ace6cbea4b8ea9e366
Сергей <privet15.89@mail.ru>
2024-09-22 14:57:22 +0500

    feat: follow/unfollow flow

--- src/app/(authorized)/search/SearchContent/actions.ts
+++ src/app/(authorized)/search/SearchContent/actions.ts
@@ -1,9 +1,5 @@
-import {
-  createFollowingOption,
-  deleteFollowerOption,
-  getUsersOptions,
-} from '@/app/lib/actionOptions';
-import { accessToken } from '@/utils/serverActions';
+import { cookies } from 'next/headers';
+import { getUsersOptions } from '@/app/lib/actionOptions';
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -13,11 +9,12 @@ export const getUsers = async (
   pageNumber: number = 1,
   cursor: number = 0
 ) => {
+  const accessToken = cookies().get('accessToken')?.value;
   const apiUrl =
     baseUrl +
     `users?search=${search}&pageSize=${pageSize}&pageNumber=${pageNumber}&cursor=${cursor}`;
   try {
-    const response = await fetch(apiUrl, getUsersOptions(accessToken()));
+    const response = await fetch(apiUrl, getUsersOptions(accessToken));
     if (!response.ok) {
       console.error('Error:', response.statusText);
       return null;
@@ -31,9 +28,10 @@ export const getUsers = async (
 };
 
 export const getUserInfo = async (userName: string) => {
+  const accessToken = cookies().get('accessToken')?.value;
   const apiUrl = baseUrl + `users/${userName}`;
   try {
-    const response = await fetch(apiUrl, getUsersOptions(accessToken()));
+    const response = await fetch(apiUrl, getUsersOptions(accessToken));
     if (!response.ok) {
       console.error('Error:', response.statusText);
       return null;
@@ -53,11 +51,12 @@ export const getUserFollowers = async (
   pageNumber: number = 1,
   cursor: number = 0
 ) => {
+  const accessToken = cookies().get('accessToken')?.value;
   const apiUrl =
     baseUrl +
     `users/${userName}/followers?search=${search}&pageSize=${pageSize}&pageNumber=${pageNumber}&cursor=${cursor}`;
   try {
-    const response = await fetch(apiUrl, getUsersOptions(accessToken()));
+    const response = await fetch(apiUrl, getUsersOptions(accessToken));
     if (!response.ok) {
       console.error('Error:', response.statusText);
       return null;
@@ -77,46 +76,12 @@ export const getUserFollowing = async (
   pageNumber: number = 1,
   cursor: number = 0
 ) => {
+  const accessToken = cookies().get('accessToken')?.value;
   const apiUrl =
     baseUrl +
     `users/${userName}/following?search=${search}&pageSize=${pageSize}&pageNumber=${pageNumber}&cursor=${cursor}`;
   try {
-    const response = await fetch(apiUrl, getUsersOptions(accessToken()));
-    if (!response.ok) {
-      console.error('Error:', response.statusText);
-      return null;
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return null;
-  }
-};
-
-export const followToUser = async (userId: number) => {
-  const apiUrl = baseUrl + `users/following`;
-  try {
-    const response = await fetch(
-      apiUrl,
-      createFollowingOption(accessToken(), userId)
-    );
-    if (!response.ok) {
-      console.error('Error:', response.statusText);
-      return null;
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return null;
-  }
-};
-
-export const unfollowByUser = async (userId: number) => {
-  const apiUrl = baseUrl + `users/follower/${userId}`;
-  try {
-    const response = await fetch(apiUrl, deleteFollowerOption(accessToken()));
+    const response = await fetch(apiUrl, getUsersOptions(accessToken));
     if (!response.ok) {
       console.error('Error:', response.statusText);
       return null;
