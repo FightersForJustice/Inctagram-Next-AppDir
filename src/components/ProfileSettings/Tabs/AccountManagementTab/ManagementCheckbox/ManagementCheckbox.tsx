9645aefb227a007dbc5319f4c89b95b99175af37
klonirovan89 <klonirovan89@gmail.com>
2024-10-02 20:56:43 +0300

    fix autoRenewal to AccountManagementTab

--- src/components/ProfileSettings/Tabs/AccountManagementTab/ManagementCheckbox/ManagementCheckbox.tsx
+++ src/components/ProfileSettings/Tabs/AccountManagementTab/ManagementCheckbox/ManagementCheckbox.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { CheckIcon } from '@radix-ui/react-icons';
-import './ManagementCheckbox.css';
+import s from './ManagementCheckbox.module.scss';
+import clsx from 'clsx';
 
 type Props = {
   autoRenewal: boolean;
@@ -11,12 +12,12 @@ type Props = {
 
 export const ManagementCheckbox: React.FC<Props> = ({ autoRenewal, title, autoRenewalHandler }) => (
   <div style={{ display: 'flex', alignItems: 'center', paddingTop: '20px' }}>
-    <Checkbox.Root className="CheckboxRoot" defaultChecked={autoRenewal} id="c1" onCheckedChange={(checked: boolean) => autoRenewalHandler(checked)}>
-      <Checkbox.Indicator className="CheckboxIndicator">
-        <CheckIcon height={22} width={22} />
+    <Checkbox.Root className={s.CheckboxRoot} defaultChecked={autoRenewal} disabled={!autoRenewal} id="c1" onCheckedChange={(checked: boolean) => autoRenewalHandler(checked)}>
+      <Checkbox.Indicator className={s.CheckboxIndicator}>
+        <CheckIcon height={22} width={22}/>
       </Checkbox.Indicator>
     </Checkbox.Root>
-    <label className="Label" htmlFor="c1">
+    <label className={clsx(s.Label, !autoRenewal && s.LabelDisabled)} htmlFor="c1">
       {title}
     </label>
   </div>
