f25179328862feebf577aaf663f07e1419839969
Pasha <klonirovan89@gmail.com>
2024-10-21 22:08:54 +0300

    fixed posts for admin posts list

--- src/components/admin/profile/photos/photos.tsx
+++ src/components/admin/profile/photos/photos.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useGetParams } from '@/utils/useGetParams';
 import { useGetCurrentUserPostsQuery } from '@/queries/posts/posts.generated';
@@ -8,27 +8,71 @@ import s from './ProfileServer.module.scss';
 import f from './Posts.module.scss';
 
 export const PhotosClient = ({ id }: { id: string }) => {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [allPostsLoaded, setAllPostsLoaded] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
   const url = useGetParams();
 
-  let currentParams = url
-    ?.slice(1)
-    .split('&')
-    .map((el) => {
-      return el.split('=');
-    });
-  const { data, loading, error, refetch } = useGetCurrentUserPostsQuery({
-    variables: currentParams?.length
-      ? {
-        userId: Number(id),
-      }
-      : { userId: Number(id),},
+  const { data, fetchMore } = useGetCurrentUserPostsQuery({
+    variables: {
+      userId: Number(id),
+      endCursorId: 0,
+    },
   });
-  //react select issue
-  //https://github.com/ndom91/react-timezone-select/issues/108
+
+  useEffect(() => {
+    if (data?.getPostsByUser?.items) {
+      setPosts(data.getPostsByUser.items);
+      setAllPostsLoaded(data.getPostsByUser.items.length < 10);
+    }
+  }, [data]);
+
+  const loadMorePosts = () => {
+    if (loadingMore || allPostsLoaded) return;
+
+    setLoadingMore(true);
+
+    const lastPostId = posts.length ? posts[posts.length - 1].id : 0;
+
+    fetchMore({
+      variables: {
+        endCursorId: lastPostId,
+      },
+    }).then((fetchMoreResult) => {
+      const newPosts = fetchMoreResult.data.getPostsByUser.items;
+
+      if (newPosts && newPosts.length > 0) {
+        setPosts((prevPosts) => {
+          const existingPostIds = new Set(prevPosts.map(post => post.id));
+          const filteredNewPosts = newPosts.filter(post => !existingPostIds.has(post.id));
+          return [...prevPosts, ...filteredNewPosts];
+        });
+      } else {
+        setAllPostsLoaded(true);
+      }
+    }).finally(() => {
+      setLoadingMore(false);
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if ((window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 300) {
+        loadMorePosts();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loadingMore, allPostsLoaded, posts]);
 
   return (
+    <div>
       <div className={s.posts}>
-        {data?.getPostsByUser?.items?.map((el) => {
+        {posts.map((el) => {
           return (
             <div key={el.id} className={f.imageContainer}>
               <Image
@@ -42,5 +86,7 @@ export const PhotosClient = ({ id }: { id: string }) => {
           );
         })}
       </div>
+      {allPostsLoaded && <div className={s.noMorePosts}>No more posts to load</div>}
+    </div>
   );
 };
