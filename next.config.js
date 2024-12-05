6122e978ad677d1222118b0586daa9df270a2225
klonirovan89 <klonirovan89@gmail.com>
2024-12-05 19:12:10 +0300

    fixed next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,11 @@ const nextConfig = {
       // 'storage.yandexcloud.net',
       'staging-it-incubator.s3.eu-central-1.amazonaws.com',
     ],
+    unoptimized: false,
   },
   env: {
     NEXT_PUBLIC_RECAPTCHA_SECRET_KEY:
-      process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY,
+    process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY,
   },
   experimental: {
     serverActions: true,
